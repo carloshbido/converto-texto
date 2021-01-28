73ae441e2ec79c38113daf6303306e9bd3f9bc7c
Carlos Bido <carloshbido@gmail.com>
2021-01-27 22:24:36 -0300

    Added new features -> Fill word and character count

--- src/scripts.js
+++ src/scripts.js
@@ -1,6 +1,7 @@
 const textAreaEl = document.querySelector('#textarea');
 const messageEl = document.querySelector('.message');
 let wordEl = document.querySelector('#words');
+let letterEl = document.querySelector('#letter');
 
 const copyBtn = document.querySelector('#copy');
 const cleanBtn = document.querySelector('#clean');
@@ -19,12 +20,29 @@ function showMessage(message) {
 //Copy value
 copyBtn.addEventListener('click', ()=> {
   console.log(textAreaEl.value);
-
-  showMessage('Mensagem de teste');
+  showMessage('Texto copiado com sucesso!');
 })
 
-textAreaEl.addEventListener('keydown', (e) => {
-  /* Colocar aqui */
+//Fill or remove total of character
+textAreaEl.addEventListener(('keydown', 'keyup'), (e) => {
+
+  if(textAreaEl.value == '') {
+    letterEl.textContent = 0;
+  } else {
+    letterEl.textContent = e.target.value.length + 1;
+  }
+})
+
+//Fill or remove total of words on the textArea
+textAreaEl.addEventListener(('keydown', 'keyup'), (e) => {
+
+  const textArr = textAreaEl.value.split(" ");
+
+  if(textAreaEl.value == '') {
+    wordEl.textContent = 0;
+  } else {
+    wordEl.textContent = textArr.length;
+  }
 })
 
 //Clean textArea
