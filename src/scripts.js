70d5f3f08b07ae4b1322135fb980d1727e09773b
Carlos Bido <carloshbido@gmail.com>
2021-01-30 14:29:09 -0300

    script.js update

--- src/scripts.js
+++ src/scripts.js
@@ -7,6 +7,7 @@ const copyBtn = document.querySelector('#copy');
 const downloadBtn = document.querySelector('#download');
 const cleanBtn = document.querySelector('#clean');
 const standardPhraseBtn = document.querySelector('#standard-phrase'); 
+const titleCaseBtn = document.querySelector('#title-case'); 
 const uppercaseBtn = document.querySelector('#uppercase');
 const lowercaseBtn = document.querySelector('#lowercase');
 const boldBtn = document.querySelector('#bold');
@@ -21,82 +22,97 @@ function showMessage(message) {
   }, 3000);
 };
 
-// Event Listeners
-//Fill or remove total of character
+
+// EVENT LISTENERS
+
+//Fill or remove total of character Event
 textAreaEl.addEventListener(('keydown', 'keyup'), (e) => {
 
-  if(textAreaEl.value == '') {
-    letterEl.textContent = 0;
-  } else {
-    letterEl.textContent = e.target.value.length + 1;
-  }
-});
-
-//Fill or remove total of words on the textArea
-textAreaEl.addEventListener(('keydown', 'keyup'), (e) => {
-
-  const textArr = textAreaEl.value.split(" ");
-
+  //Word
+  const textArr = textAreaEl.value.split(' ');
   if(textAreaEl.value == '') {
     wordEl.textContent = 0;
   } else {
     wordEl.textContent = textArr.length;
   }
+
+  //Caracter/Letter
+  if(textAreaEl.value == '') {
+    letterEl.textContent = 0;
+  } else {
+    letterEl.textContent = e.target.value.length + 1;
+  }
+
 });
 
-//Copy value
+//Copy value Event
 copyBtn.addEventListener('click', ()=> {
   //Implementar
   showMessage('Texto copiado com sucesso!');
 });
 
-//Download value
+//Download value Event
 downloadBtn.addEventListener('click', ()=> {
   //Implementar
   console.log(textAreaEl.value);
 });
 
-//Clean textArea
+//Clean textArea Event
 cleanBtn.addEventListener('click', () => {
   textAreaEl.value = '';
 });
 
-//Standard text
-standardPhraseBtn.addEventListener('click', () => {
-  // const firstLetter = textAreaEl.value[0].toUpperCase();
-  // const textWithoutFirstLetter = textAreaEl.value.split(" ").splice(0,1);
+//Title Case Event
+titleCaseBtn.addEventListener('click', () => {
 
-  // console.log(firstLetter, textWithoutFirstLetter);
+  let newArray = [];
+
+  //Put the all words in loowerCase and transform in Arr
+  const text = textAreaEl.value.toLowerCase();
+  const textArr = text.split(' ');
+
+  console.log(textArr);
+
+  // Catch all words, put the first letter in Uppercase and concat 
+  textArr.forEach(letter => {
+    newArray += letter[0].toUpperCase() + letter.slice(1) + ' ';
+  });
+
+  //Put the value on TextArea
+  textAreaEl.value = newArray;
+
 });
 
-//UpperCase 
+//UpperCase Event
 uppercaseBtn.addEventListener('click', () => {
   textAreaEl.value = textAreaEl.value.toUpperCase();
 });
 
-//LowerCase 
+//LowerCase Event
 lowercaseBtn.addEventListener('click', () => {
   textAreaEl.value = textAreaEl.value.toLowerCase();
 });
 
-//Bold 
+//Bold Event
 boldBtn.addEventListener('click', () => {
   textAreaEl.classList.toggle('bold');
   let index;
 
   textAreaEl.classList.contains('bold') ? index = 1 : index = 0;
 
+  //Changes the naming button
   const nameBtn = ['Negrito', 'Retirar Negrito']
   boldBtn.innerHTML = `<strong>${nameBtn[index]}</strong>`
 });
 
-//Italic 
+//Italic Event
 italicBtn.addEventListener('click', () => {
   textAreaEl.classList.toggle('italic');
   let index;
 
   textAreaEl.classList.contains('italic') ? index = 1 : index = 0;
 
+  //Changes the naming button
   const nameBtn = ['Itálico', 'Retirar Itálico']
   italicBtn.innerHTML = `<i>${nameBtn[index]}</i>`
 });
